84ae6748bda61b4f6a77d4f42aedfad5dee6ba32
feltsSite <mr.skeleton16@gmail.com>
2017-08-13 18:21:34 +0300

    Signed-off-by: feltsSite <mr.skeleton16@gmail.com>

---
+++ script/main.js
@@ -0,0 +1,20 @@
+$(function() {
+		var mMnu = true;
+	$('button.toggle_mnu').click(function() {
+		if (mMnu) {
+			$('.navi').fadeIn(500);
+			$('.sw-stick').css("width", "0");
+			$('.sandwich').addClass('active');
+			mMnu = false;
+		} else {
+			$('.navi').fadeOut(300);;
+			$('.sandwich').removeClass('active');
+			$('.sw-stick').delay(1000).css("width", "25px")
+			mMnu = true;
+		}
+	});
+});
+
+$(window).on('load', function() {
+	$('.preloader').delay(1000).fadeOut('slow');;
+});
